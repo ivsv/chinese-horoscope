7320ad407aa26dd43027cfd7e5f3089b0680262a
Ivan Shokurov <ivanukas@icloud.com>
2016-03-19 17:14:09 +1000

    Update chinese_horoscope.sh

--- chinese_horoscope.sh
+++ chinese_horoscope.sh
@@ -8,37 +8,17 @@ read user_year
 # checking for validation input data
 if [ ${#user_year} -ne 4 ]
 	
-then
-	# in case of true
+	then
+	# in case of false
 	echo "Incorrect Data!"
 
-else
-	# in case of false 
+	else
+	# in case of true 
 	declare -i residue_for_animals=$((user_year%12))
 	declare -i residue_for_colours=$((user_year%10/2))
-
-	case $residue_for_animals in          # residues below:
-		0 ) result1=${animals[0]};;   #0
-		1 ) result1=${animals[1]};;   #1
-		2 ) result1=${animals[2]};;   #2
-		3 ) result1=${animals[3]};;   #3
-		4 ) result1=${animals[4]};;   #4
-		5 ) result1=${animals[5]};;   #5
-		6 ) result1=${animals[6]};;   #6
-		7 ) result1=${animals[7]};;   #7
-		8 ) result1=${animals[8]};;   #8
-		9 ) result1=${animals[9]};;   #9
-		10 ) result1=${animals[10]};; #10
-		11 ) result1=${animals[11]};; #11
-	esac
-
-	case $residue_for_colours in
-		0 ) result2=${colours[0]};;   #0
-		1 ) result2=${colours[1]};;   #1
-		2 ) result2=${colours[2]};;   #2
-		3 ) result2=${colours[3]};;   #3
-		4 ) result2=${colours[4]};;   #4
-	esac
+	# indexes of arrays above are equivalent to residue after operations in 17th and 18th lines..
+	result1=${animals[residue_for_animals]}
+	result2=${colours[residue_for_colours]}
 
 fi
 # concatenation of the strings in output
